fbdde2c905b7ad5d7dedef854cd09b6e63634cb0
Peter <peter@Cvoky.local>
2017-12-01 00:06:49 +0100

    Lesson 2 in progress. Using mega-shiny structure.

--- Preparation/data_lesson2_tmp.R
+++ Preparation/data_lesson2_tmp.R
@@ -6,7 +6,9 @@ data_lesson2 <- data.frame(
   dimension2=rbinom(1000, 100, 0.1),
   dimension3=rbinom(1000, 100, 0.1),
   premium=rlnorm(1000, meanlog = 1, sdlog = 0.4)* 1000000,
-  loss=rgamma(1000, 500) * 1000000
+  expenses=rlnorm(1000, meanlog = 1, sdlog = 0.4)* 100000,
+  loss=rgamma(1000, 500) * 1000000,
+  date=as.Date("2000-01-01") + 0:999
 )
 
 saveRDS(data_lesson2, "data/data_lesson2.rds")
