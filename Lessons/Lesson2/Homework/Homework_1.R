3a81f6728ddfdc33c2883fa0ab6810c402659de5
veronikagoceljakova <47779413+veronikagoceljakova@users.noreply.github.com>
2019-03-05 01:21:34 +0100

    Update Homework_1.R

--- Lessons/Lesson2/Homework/Homework_1.R
+++ Lessons/Lesson2/Homework/Homework_1.R
@@ -3,32 +3,37 @@
 # __Commit__ it to your repository into `Lessons/Lesson2/Homework`.
 
 ## Code
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
+library(dplyr)
+library(ggplot2)
+dt_KPI_raw <- read.csv("lesson2_KPI.csv")
 
 
+dt_KPI_raw %>% 
+  mutate(Premium = ifelse(Premium < 0, 0, Premium))
+
+
+dt_KPI_raw %>%  mutate(UWR = Premium - Expenses - Losses) %>% 
+  group_by(Unit) %>% 
+  summarize(UWR = sum(UWR, na.rm = TRUE)) %>% 
+  arrange(UWR) #najprofitabilnejsie - 7
+
+dt_KPI_raw %>%  mutate(UWR = Premium - Expenses - Losses) %>% 
+  filter(Unit == "Unit7") %>%
+  group_by(Year) %>% 
+  summarize(UWR = sum(UWR, na.rm = TRUE)) %>% 
+  arrange(UWR)
+
+dt_KPI_raw %>% 
+  mutate(UWR = Premium - Expenses - Losses) %>% 
+  filter(Unit == "Unit7") %>%
+  group_by(Year) %>% 
+  summarize(UWR = sum(UWR, na.rm = TRUE)) %>% 
+  ggplot(aes(x = reorder(Year, UWR), y = UWR)) + 
+  geom_col()
 
 
 # Your Explanation about analysis:
-# 
-# 
-# 
+# Ocistila som data od zap. poistneho, podla hodiny vyslo portfolio 7 
+#ako najprofitabilnejsie, preto som dalej pracovala len s tymito datami,
+#ktore som zoskupila podla rokov a najlepsi rok vysiel rok 2013, kedze
+#UWR pre tento rok vyslo najvyssie
