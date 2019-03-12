00d3a1b0f494a736d6ee2665cfb30fef064ed4c4
veronikagoceljakova <47779413+veronikagoceljakova@users.noreply.github.com>
2019-03-12 01:37:03 +0100

    Add files via upload

--- Lessons/Lesson3/Homework/Shiny/ui.R
+++ Lessons/Lesson3/Homework/Shiny/ui.R
@@ -1,37 +1,31 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+setwd("D:/aktuarstvo/GeneralInsurance_Class/Data")
+dt_KPI <- read.csv("lesson2_KPI.csv")
+dt_KPI <- dt_KPI %>% filter_all(all_vars(!is.na(.)))
+
+ui <- fluidPage(
+  titlePanel("Premium vs. Expenses"),
+  sidebarLayout(      
+   sidebarPanel(
+      selectInput("select", "Colouring Var: ", 
+                  choices=list("Region", "Unit", "Segment", "Business", "Year")),
+      hr(),
+      helpText("KPI data")
+    ),
+  
+    mainPanel(
+      plotOutput("Plot")  
+    )
+    
+  )
+  
+)
 
 
-#### MAIN PAGE ####
-ui <- fluidPage(theme = shinytheme("paper"),
-                title = "General Insurance Class",
-                navbarPage(
-                  title = span(
-                    img(src = "logo_zurich.png", height = 30),
-                    "General Insurance Class"
-                  ),
-                  tabPanel("lesson3",
-                           tagList(
-                             fluidRow(div(
-                               style = "text-align:center", h3("This is second class about KPIs")
-                             )),
-                             navlistPanel(widths = c(2, 6),
-                                          tabPanel(
-                                            "Focus Group",
-                                            fluidRow(column(
-                                              width = 3,
-                                              offset = 9,
-                                              uiOutput("lesson3_KPI_multidim_select_axis_render")
-                                            )),
-                                            fluidRow(
-                                              column(width = 9,
-                                                     DT::dataTableOutput("lesson3_KPI_multidim_table")),
-                                              column(
-                                                width = 3,
-                                                plotOutput("lesson3_KPI_multidim_ratio_graph",
-                                                           width = 400),
-                                                plotOutput("lesson3_KPI_multidim_UWR_graph",
-                                                           width = 400)
-                                              )
-                                            )
-                                          ))
-                           ))
-                ))
+#slovny popis:
+#pri pohlade na Region z grafu vidime, ze zo zaciatku su vsetky regiony indiferentne, co sa nakladovosti/ziskov tyka, ale potom region Alandia bude pravdepodobne najvacsim, co sa poctu poistenych tyka, lebo hoci dosahuje najvacsie zisky, ale aj najvacsie naklady kvoli rychlemu rastu smernice
+#z grafu Unit sa da vidiet, ze hlavnou skupinou poistenych bude Unit2 podla podobnej uvahy
+#zo Segmentoveho grafu sa dozvedame, ze poistovna sa zameriava skor na maly segment, alebo mozno budu naklady aj zisky preto take vysoke oproti velkemu segmentu, lebo sa v tomto segmente stavaju poistne udalosti castejsie a aj preto je poistne vyssie(predp. z vyssich ziskov), nakladovy graf velkeho segmentu je prevazne pod grafom maleho segmentu, a teda je menej nakladny  
+#z grafu pre Business mozeme usudit, ze najziskovejsou bude oblast travel a potom housing, avsak pri domacnostiach naklady rastu rychlejsie kvoli strmsiemu sklonu
